2a5b7b214d783407f66c674b15322bb0487445bd
phamlam2k <mailam1309@gmail.com>
2023-12-17 22:49:36 +0700

    Update api

--- src/hooks/useQuestionAction.js
+++ src/hooks/useQuestionAction.js
@@ -1,12 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { saveInteractionsApi } from "../utils/api";
 import { message } from "antd";
+import { QUERY_KEYS } from "../config/keys";
 
 const useQuestionAction = () => {
+  const queryClient = useQueryClient();
   const saveInteractions = useMutation({
     mutationFn: (data) => saveInteractionsApi(data),
     onSuccess: (data) => {
       message.success(data.data.message);
+
+      queryClient.invalidateQueries([QUERY_KEYS.QUESTION_LIST]);
     },
     onError: () => {
       message.error("Interaction created failed");
